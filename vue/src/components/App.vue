1d4ba76761746156efa90dc225704e0ba19f8355
VsevolodMu <Vsevolod-2003@yandex.ru>
2024-02-22 12:31:59 +0300

    Button ready

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import CustomButton from "@/components/CustomButton.vue";
+import CustomButton from "@/components/CustomButtonSixth.vue";
 
 export default {
   components: {
